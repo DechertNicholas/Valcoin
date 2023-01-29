d88a9d9d0db8d0d9e23e49cd06afc8df3d078d27
Nicholas Dechert <dechertnicholas@gmail.com>
2023-01-29 14:16:14 -0800

    Add script for building locally

---
+++ BuildLocal.ps1
@@ -0,0 +1,10 @@
+$env:Solution_Name = "Valcoin.sln"
+$env:Solution_Name = "Valcoin"
+$env:Configuration = "Debug"
+$env:Platform = "x64"
+$env:Appx_Package_Build_Mode = "SideloadOnly"
+$env:Appx_Bundle = "Never"
+$env:CertificatePath = Resolve-Path .\Valcoin\Valcoin_TemporaryKey.pfx
+$env:Appx_Package_Dir = "Packages\"
+
+dotnet msbuild $env:Solution_Name /p:Configuration=$env:Configuration /p:Platform=$env:Platform /p:UapAppxPackageBuildMode=$env:Appx_Package_Build_Mode /p:AppxBundle=$env:Appx_Bundle /p:PackageCertificateKeyFile="$(Split-Path $env:CertificatePath -Leaf)" /p:AppxPackageDir="$env:Appx_Package_Dir" /p:GenerateAppxPackageOnBuild=true
