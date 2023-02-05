e47f86d11e5ff83e3ee7177da0e7264cd83bf6df
Nicholas Dechert <dechert.nicholas@gmail.com>
2023-02-04 22:51:51 -0800

    Beginning of the transaction model

---
+++ Valcoin/Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valcoin.Models
+{
+    internal class Transaction
+    {
+        public string TxId { get; set; }
+
+        public string PreviousTxId { get; set; }
+
+        public string SenderAddress { get; set; }
+
+        public string ReceiverPublicKey { get; set; }
+
+        public int Amount { get; set; }
+
+        public byte[] Signature { get; set; }
+
+        // use inputs and outputs as bitcoin does, create hash models for this data.
+    }
+}
