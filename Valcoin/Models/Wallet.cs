afc7bd1eb89e8ee9409dafd62be3c0bb2f3fab3f
Nicholas Dechert <dechert.nicholas@gmail.com>
2023-02-11 10:30:29 -0800

    Remove WalletId in favor of non-computed string Address

--- Valcoin/Models/Wallet.cs
+++ Valcoin/Models/Wallet.cs
@@ -13,19 +13,11 @@ namespace Valcoin.Models
 {
     public class Wallet
     {
-
-        /// <summary>
-        /// Specifically for Entity Framework Core tracking. Not used in the Valcoin protocol.
-        /// </summary>
-        public int WalletId { get; private set; }
-
         /// <summary>
         /// Hex string representation of the hashed public key, for readability by people. Prefexed by '0x' to know that it is an address and not a block or transaction hash.
         /// </summary>
-        public string Address
-        {
-            get => GetAddressAsString();
-        }
+        [Key]
+        public string Address { get; set; }
 
         /// <summary>
         /// The raw bytes of the hashed public key.
@@ -48,6 +40,9 @@ namespace Valcoin.Models
         [NotMapped]
         private readonly RSA _rsa;
 
+        /// <summary>
+        /// Creates a new wallet with a new key pair.
+        /// </summary>
         public Wallet()
         {
             _rsa = RSA.Create();
@@ -55,8 +50,14 @@ namespace Valcoin.Models
             PublicKey = _rsa.ExportRSAPublicKey();
             PrivateKey = _rsa.ExportRSAPrivateKey(); // TODO: Decide if this will be encrypted
             AddressBytes = SHA256.Create().ComputeHash(PublicKey);
+            Address = Convert.ToHexString(AddressBytes);
         }
 
+        /// <summary>
+        /// Creates a wallet with existing key pair. Used for loading from the database.
+        /// </summary>
+        /// <param name="publicKey"></param>
+        /// <param name="privateKey"></param>
         public Wallet(byte[] publicKey, byte[] privateKey)
         {
             _rsa = RSA.Create();
@@ -67,18 +68,34 @@ namespace Valcoin.Models
             AddressBytes = SHA256.Create().ComputeHash(publicKey);
         }
 
+        /// <summary>
+        /// Returns the AddressBytes as a hex string, prefixed with '0x' to signify that it is an address.
+        /// </summary>
+        /// <returns></returns>
         public string GetAddressAsString()
         {
             return "0x" + Convert.ToHexString(AddressBytes);
         }
 
+        /// <summary>
+        /// Signs data using the wallet's key pair.
+        /// </summary>
+        /// <param name="data">The data to sign. Normally used in a <see cref="TxOutput"/>'s <see cref="TxOutput.LockSignature"/>.</param>
+        /// <returns></returns>
         public byte[] SignData(byte[] data)
         {
             return _rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
         }
 
+        /// <summary>
+        /// Verifies data that was previously signed. 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="signature"></param>
+        /// <returns></returns>
         public bool VerifyData(byte[] data, byte[] signature)
         {
+            // TODO: Load the public key of the transaction since it won't always be our public key
             return _rsa.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
         }
     }
